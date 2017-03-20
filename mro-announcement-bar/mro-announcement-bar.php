02593333b407f785b874902d1a3be89f50b55664
MatiRosero <gekidasa@gmail.com>
2017-03-19 23:53:14 -0600

    initial commit

---
+++ mro-announcement-bar/mro-announcement-bar.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: MRo Annoucement Bar
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Sets up custom post types for Yoga Mandir.
+Version: 1.0
+Author: Mat Rosero
+Author URI: http://2tabbies.com
+Plugin Type: Piklist
+License: GPL2
+*/
+?>
