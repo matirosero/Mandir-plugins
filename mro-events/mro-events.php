a47b4e64ee26e7af97ac775c12d9b115ba891e48
MatiRosero <gekidasa@gmail.com>
2017-04-05 15:58:02 -0600

    readded number validadtion to events

--- mro-events/mro-events.php
+++ mro-events/mro-events.php
@@ -73,3 +73,15 @@ function mro_events_post_types($post_types) {
 
 	return $post_types;
 }
+
+
+// Validation for valid number
+add_filter('piklist_validation_rules', 'check_valid_number', 11);
+function check_valid_number($validation_rules) {
+  $validation_rules['valid_number'] = array(
+    'rule' => "/[-+]?[0-9]*[.,]?[0-9]+/",
+    'message' => __('is not a number', 'mro-events')
+  );
+
+  return $validation_rules;
+}
