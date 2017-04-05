b8a5750a4fe9dcec5a262ebcf38da2fed40db1eb
MatiRosero <gekidasa@gmail.com>
2017-04-05 16:00:11 -0600

    readded events/thecontent formerly details

---
+++ mro-events/parts/meta-boxes/thecontent-meta.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Title: Detalles
+Post Type: mro-event
+Context: normal
+Priority: high
+Order: 2
+*/
+
+piklist('field', array(
+  'type' => 'editor',
+  'template'=>'field',
+  'scope' => 'post',
+  'field' => 'post_content',
+  'options' => array (
+    'wpautop' => true,
+    'media_buttons' => true,
+    'tabindex' => '',
+    'editor_css' => '',
+    'editor_class' => '',
+    'teeny' => false,
+    'dfw' => false,
+    'tinymce' => true,
+    'quicktags' => true,
+  )
+));
