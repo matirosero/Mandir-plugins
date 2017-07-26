e3e83991f86c068cff39ee9d1cbb3761d52ed14f
MatiRosero <gekidasa@gmail.com>
2017-07-26 16:54:28 -0600

    cleanup

--- mro-pages/inc/general-functions.php
+++ mro-pages/inc/general-functions.php
@@ -21,3 +21,34 @@ function mro_the_content() {
 	  )
 	));
 }
+
+
+/*
+function mro_intro_after_title() {
+	$current_screen = get_current_screen();
+  	if( $current_screen ->id === "page" ) {
+
+		piklist('field',
+		  array(
+		    'type' => 'editor',
+		    'field' => 'mro_page_intro',
+		    'template'=>'field',
+		    'label' => __('Introduction', 'mro-pages'),
+		    'options' => array(
+		      'wpautop' => true,
+		      'media_buttons' => false,
+		      'teeny' => false,
+		      'textarea_rows' => 5,
+		      'drag_drop_upload' => false,
+		      'tinymce' => array(
+		        'resize' => false,
+		        'wp_autoresize_on' => true,
+		      ),
+		      'editor_height' => 100,
+		    ),
+		));
+	}
+}
+add_action('edit_form_after_title', 'mro_intro_after_title');
+*/
+
