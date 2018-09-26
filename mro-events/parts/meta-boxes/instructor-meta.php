7a5778062dc92cec53dcb5daf9990fac29ae2f48
MatiRosero <gekidasa@gmail.com>
2018-09-26 10:42:58 -0600

    remove extra stuff

--- mro-events/parts/meta-boxes/instructor-meta.php
+++ mro-events/parts/meta-boxes/instructor-meta.php
@@ -7,36 +7,6 @@ Priority: high
 Order: 2
 */
 
-// piklist('field', array(
-//     'type' => 'post-relate'
-//     ,'scope' => 'mro-team'
-//     ,'template' => 'field'
-//   ));
-
-//  piklist('field', array(
-//     'type' => 'checkbox',
-//     'field' => 'mro_event_instructor',
-//     'label' => __('Event instructor', 'mro-events'),
-//     'choices' => piklist(
-//       get_posts(array(
-//         'post_type' => 'mro-team',
-//         'numberposts' => -1,
-//         'orderby' => 'title',
-//         'order' => 'ASC',
-//         'tax_query' => array(
-// 					array(
-// 						'taxonomy' => 'mro_team_tax',
-// 						'field'    => 'slug',
-// 						'terms'    => 'taller',
-// 					),
-// 				),
-//       )),
-//       array('ID', 'post_title')
-//     ),
-//     'relate' => array(
-//       'scope' => 'post'
-//     )
-//   ));
 
   piklist('field', array(
     'type' => 'checkbox',
@@ -64,14 +34,3 @@ Order: 2
       'multiple' => 'multiple'
     )
   ));
-
-
-  //Select teacher
-  // piklist('field', array(
-  //   'type' => 'select',
-  //   'field' => 'mro_event_instructor_test',
-  //   'label' => __('Choose teacher', 'mro-pages'),
-  //   'columns' => 4,
-  //   'choices' => mro_training_get_teachers( 'taller' ),
-  // ));
- 
